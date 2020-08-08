dd0270d55ed396405591f658d3c13eac32b6f81f
lkhang <socbaycff@gmail.com>
2020-08-08 09:41:58 +0700

    update

--- UserNotification/AppDelegate.swift
+++ UserNotification/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
 
@@ -35,3 +35,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert])
+    }
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("da nhan")
+        completionHandler()
+    }
+}
--- UserNotification/ViewController.swift
+++ UserNotification/ViewController.swift
@@ -1,20 +1,71 @@
-//
-//  ViewController.swift
-//  UserNotification
-//
-//  Created by L Khang on 7/31/20.
-//  Copyright © 2020 L Khang. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        kiemTraDaDcCapQuyenChua()
     }
+    
+    func kiemTraCacPendingNotif() {
+        UNUserNotificationCenter.current().getPendingNotificationRequests { (pendings) in
+            
+        }
+    }
+    
+    func chayThongBao() {
+        // tao con tent
+        let content = UNMutableNotificationContent()
+        content.title = "Tua de"
+        content.body = "noi dung"
+        content.sound = .default
+        content.badge = 0
+        content.subtitle = "tua de phu"
 
-
+        //tao trigger (chay notification sau 3s, khong lap lai)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        //tap request
+        let request = UNNotificationRequest(identifier: "id", content: content, trigger: trigger)
+        // up request len
+        UNUserNotificationCenter.current().add(request) { (error) in
+            guard error == nil else { return }
+            
+            print("Notification scheduled!")
+        }
+    }
+    
+    
+    func kiemTraDaDcCapQuyenChua() {
+        // goi ham lay setting notification (getNotifcationSetting)
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            let status = settings.authorizationStatus //lay trang thai uy quyen
+            
+            switch status {
+            case .authorized, .provisional: // da uy quyen
+                print("bat dau thong bao")
+                self.chayThongBao()
+            case .notDetermined: // chua xac dinh
+                print("xin cap quyen thong bao")
+                self.xinQuyenThongBao()
+                
+            default: break
+            }
+        }
+    }
+    
+    // xin quyen thong bao
+    func xinQuyenThongBao() {
+        // goi ham yeu cau uy quyen (requestAuthorization)
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.announcement,.badge]){ (isGranted, error) in
+            if error != nil {return}
+            if isGranted {
+                print("dc cap quyen")
+                print("chay chuc nang")
+                self.chayThongBao()
+            } else {
+                print("k dc cap quyen")
+            }
+        }
+    }
 }
 
